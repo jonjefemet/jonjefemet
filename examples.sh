79b8266ed470a8af17032fd2dafe75b07e7d581d
jonjefemet <jonshua_97@outlook.com>
2021-02-24 09:07:46 -0500

    Create examples.sh

---
+++ examples.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Nombre:"
+read -r name
+
+content=$(wget -q -O - "https://lectortmo.com/library?_page=1&title=${name/ /+}")
+
+echo $content | grep -oP "(?<=<h4 class=\"text-truncate\" title=\").*?(?=\">)" > names.txt
+
+array=()
+while read -r line; do
+    # Reading each line
+    array+=("$line")
+done <names.txt
+
+show_menus() {
+    n=1
+    for line in "${array[@]}"; do
+        echo $line
+        echo $n
+        n=$((n + 1))
+    done
+}
+
+show_menus
